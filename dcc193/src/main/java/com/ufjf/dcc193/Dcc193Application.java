4f45659c86f0e2916a12760d25f182ec22539c5e
Rodrigo Pituba De Souza <rpsouza@stefanini.com>
2019-04-22 14:34:30 -0300

    Criando projeto com as dependencias

---
+++ dcc193/src/main/java/com/ufjf/dcc193/Dcc193Application.java
@@ -0,0 +1,13 @@
+package com.ufjf.dcc193;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dcc193Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dcc193Application.class, args);
+	}
+
+}
---
+++ dcc193/src/test/java/com/ufjf/dcc193/Dcc193ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ufjf.dcc193;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Dcc193ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
